5b15e42abd7ca4bfef1258871383cc90c6566ba5
Dan28396 <Dan24335>
2020-02-05 00:56:49 +0300

    orders route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,7 @@ module.exports = function (app, db) {
     app.route('/orders')
         .post(function (req, res) {
             const order = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                address: req.body.address,
-                city: req.body.city,
-                country: req.body.country,
-                region: req.body.region,
-                zip: req.body.zip,
-                items: req.body.items
+                metadata: req.body.metadata.jopa,
             };
             db.collection('orders').insert(order, (err, result) => {
                 if (err) {

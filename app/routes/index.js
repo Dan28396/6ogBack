8ec3a74108d8d6077dff9691e645cbd156940823
Dan28396 <Dan24335>
2020-02-09 21:22:51 +0300

    almost done

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,16 +6,42 @@ const db = require('../config/db')
 
 router.post('/api/yandex', async (req, res) => {
     const orders = await loadOrdersCollection();
-    await orders.findOneAndReplace({"object.id" : req.body.object.id}, req.body,{upsert: true});
+    await orders.findOneAndReplace({"id": req.body.object.id},
+        {
+            "id": req.body.object.id,
+            "status": req.body.object.status,
+            "created_at": req.body.object.created_at,
+            "country": req.body.object.metadata.country,
+            "address": req.body.object.metadata.address,
+            "city": req.body.object.metadata.city,
+            "postalCode": req.body.object.metadata.postalCode,
+            "name": req.body.object.metadata.name,
+            "email": req.body.object.metadata.email,
+            "order": JSON.parse(req.body.object.metadata.order),
+            "source": "yandex"
+        }, {upsert: true});
     res.status(200).send();
 })
 router.get('/api/orders', async (req, res) => {
     const orders = await loadOrdersCollection();
-        res.send(await orders.find({}).toArray());
+    res.send(await orders.find({}).toArray());
 });
 router.post('/api/paypal', async (req, res) => {
     const orders = await loadOrdersCollection();
-    await orders.insertOne(req.body);
+    await orders.insertOne({
+            "id": req.body.order.id,
+            "status": req.body.order.status,
+            "created_at": req.body.order.create_time,
+            "country": req.body.order.purchase_units[0].shipping.address.country_code,
+            "address": req.body.order.purchase_units[0].shipping.address.address_line_1,
+            "city": req.body.order.purchase_units[0].shipping.address.admin_area_2,
+            "postalCode": req.body.order.purchase_units[0].shipping.address.postal_code,
+            "name": req.body.order.purchase_units[0].shipping.name.full_name,
+            "email": req.body.order.payer.email_address,
+            "order": req.body.order.purchase_units[0].items,
+            "source": "paypal"
+        }
+    );
     res.status(200).send();
 })
 
